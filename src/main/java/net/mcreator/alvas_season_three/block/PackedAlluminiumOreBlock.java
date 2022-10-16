27bc9cf7a3e624acd425ae559fe05b78581caf00
AlexNompe <Alex.Nompe@yandex.ru>
2022-10-16 08:11:54 +0300

    replacement of placeholders completed

---
+++ src/main/java/net/mcreator/alvas_season_three/block/PackedAlluminiumOreBlock.java
@@ -0,0 +1,77 @@
+
+package net.mcreator.alvas_season_three.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.alvas_season_three.itemgroup.AlvasSeason3ItemGroup;
+import net.mcreator.alvas_season_three.item.AlluminiumIngotItem;
+import net.mcreator.alvas_season_three.AlvasSeason3ModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AlvasSeason3ModElements.ModElement.Tag
+public class PackedAlluminiumOreBlock extends AlvasSeason3ModElements.ModElement {
+	@ObjectHolder("alvas_season_3:packed_alluminium_ore")
+	public static final Block block = null;
+
+	public PackedAlluminiumOreBlock(AlvasSeason3ModElements instance) {
+		super(instance, 12);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(AlvasSeason3ItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutoutMipped());
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(4f, 3f).setLightLevel(s -> 0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool().notSolid().setOpaque((bs, br, bp) -> false));
+			setRegistryName("packed_alluminium_ore");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 0;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(AlluminiumIngotItem.block));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/alvas_season_three/item/AlluminiumSwordItem.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.alvas_season_three.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.alvas_season_three.AlvasSeason3ModElements;
+
+@AlvasSeason3ModElements.ModElement.Tag
+public class AlluminiumSwordItem extends AlvasSeason3ModElements.ModElement {
+	@ObjectHolder("alvas_season_3:alluminium_sword")
+	public static final Item block = null;
+
+	public AlluminiumSwordItem(AlvasSeason3ModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 300;
+			}
+
+			public float getEfficiency() {
+				return 5.5f;
+			}
+
+			public float getAttackDamage() {
+				return 4f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 15;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(AlluminiumIngotItem.block));
+			}
+		}, 3, -2.4f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("alluminium_sword"));
+	}
+}
